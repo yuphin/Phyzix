4b3f029f6bcce09fe8620f17abdac744b675f33a
yuphin <yunus.c.cakir@gmail.com>
2020-12-02 22:04:29 +0300

    Updated the project for Exercise 2, moved rigid body sim to other branch

--- Simulations/main.cpp
+++ Simulations/main.cpp
@@ -21,8 +21,8 @@ using namespace GamePhysics;
 //#define ADAPTIVESTEP
 
 //#define TEMPLATE_DEMO
-#define MASS_SPRING_SYSTEM
-//#define RIGID_BODY_SYSTEM
+//#define MASS_SPRING_SYSTEM
+#define RIGID_BODY_SYSTEM
 //#define SPH_SYSTEM
 
 #ifdef TEMPLATE_DEMO
@@ -380,7 +380,9 @@ int main(int argc, char* argv[])
 	DXUTSetCursorSettings( true, true ); // Show the cursor and clip it when in full screen
 	DXUTCreateWindow( L"Demo" );
 	DXUTCreateDevice( D3D_FEATURE_LEVEL_11_0, true, 1280, 960 );
+#ifdef MASS_SPRING_SYSTEM
 	((MassSpringSystemSimulator*) g_pSimulator)->init_resources(g_pDUC->g_ppd3Device);
+#endif
 	DXUTMainLoop(); // Enter into the DXUT render loop
 	delete g_pSimulator;
 	DXUTShutdown(); // Shuts down DXUT (includes calls to OnD3D11ReleasingSwapChain() and OnD3D11DestroyDevice())
